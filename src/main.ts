3b559f5b8582cdfe57eddcfafab59bf0d4edcf85
DivakarSS <69146417+DivakarSS@users.noreply.github.com>
2023-07-07 18:19:55 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,4 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
-import BootstrapVueNext from 'bootstrap-vue-next'
-createApp(App).use(BootstrapVueNext).use(router).mount('#app')
+createApp(App).use(router).mount('#app')
